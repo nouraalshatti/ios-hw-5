2363ef840a0623a4c30db3273e35bce8eb7db474
نوره الشطي <70806444+nouraalshatti@users.noreply.github.com>
2020-10-19 15:34:03 +0300

    update1

---
+++ To-do/To-do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-do
+//
+//  Created by Noura on 10/15/20.
+//  Copyright © 2020 Noura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ To-do/To-do/welcomescreenVC.swift
@@ -0,0 +1,47 @@
+//
+//  welcomescreenVC.swift
+//  To-do
+//
+//  Created by Noura on 10/15/20.
+//  Copyright © 2020 Noura. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+
+class welcomescreenVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "clouds", ofType: "mp4")!))
+//        let layer = AVPlayerLayer(player: player)
+//        layer.frame = view.bounds
+//        view.layer.addSublayer(layer)
+//        layer.videoGravity = .resizeAspectFill
+//        player.play() 
+//       
+//        
+//        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
